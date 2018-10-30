74df688045c613986b7d95f52e4929e5cbb3e0e4
J Nielsen <buhros@hotmail.com>
2018-10-30 12:49:50 +0100

    CurrentlyWorking
    before we break it

--- BattleShipWpfApp/MainWindow.xaml.cs
+++ BattleShipWpfApp/MainWindow.xaml.cs
@@ -21,10 +21,12 @@ namespace BattleShipWpfApp
     public partial class MainWindow : Window
     {
         private int _missCounter = 1;
+        private int _hitCounter = 0;
         int gridSize = 10;
         String[,] _gridArray;
         Button[,] _buttonArray;
         private Random random = new Random();
+        List<Point> shipList = new List<Point>();
 
         public MainWindow()
         {
@@ -32,21 +34,35 @@ namespace BattleShipWpfApp
             _gridArray = new String[gridSize, gridSize];
             _buttonArray = new Button[gridSize, gridSize];
 
-<<<<<<< HEAD
-            CreateShip(2);
-            CreateShip(3);
-            CreateShip(4);
-            CreateShip(5);
-=======
-            //MOVED
-            List<Point> shipList = new List<Point>();
+            CreateGrid();
+            CreateButtons();
 
             CreateShip(2, shipList);
             CreateShip(3, shipList);
             CreateShip(4, shipList);
             CreateShip(5, shipList);
->>>>>>> origin/NKraghsBranch
+        }
 
+        private void CreateButtons()
+        {
+            for (int x = 0; x < gridSize; x++) //>>
+            {
+                for (int y = 0; y < gridSize; y++) //VV
+                {
+                    Button btn = new Button();
+                    btn.Click += GridClicked(x, y);
+                    btn.Content = "";
+                    _buttonArray[x, y] = btn;
+                    btn.Margin = new Thickness(2, 2, 2, 2);
+                    ViewGrid.Children.Add(btn);
+                    Grid.SetColumn(btn, x);
+                    Grid.SetRow(btn, y);
+                }
+            }
+        }
+
+        private void CreateGrid()
+        {
             for (int i = 0; i < gridSize; i++)
             {
                 RowDefinition rd = new RowDefinition(); //generating grid/row defitions automatically per grid size
@@ -57,39 +73,20 @@ namespace BattleShipWpfApp
                 cd.Width = new GridLength(50, GridUnitType.Pixel);
                 ViewGrid.ColumnDefinitions.Add(cd);
             }
-
-            for (int x = 0; x < gridSize; x++) //>>
-            {
-                for (int y = 0; y < gridSize; y++) //VV
-                {
-                    //2x1, 3x1, 4x1, 5x1 no overlap, no diagonal
-
-
-                    Button btn = new Button();
-                    btn.Click += GridClicked(x, y);
-                    btn.Content = "btn";
-                    _buttonArray[x, y] = btn;
-                    btn.Margin = new Thickness(2, 2, 2, 2);
-                    ViewGrid.Children.Add(btn);
-                    Grid.SetColumn(btn, x);
-                    Grid.SetRow(btn, y);
-                }
-            }
         }
 
         private void CreateShip(int size, List<Point> shipList)
         {
             var randomYIndex = random.Next(0, gridSize); //random.Next(min,max) min included, max excluded
             var randomXIndex = random.Next(0, gridSize);
+
             bool direction = random.NextDouble() >= 0.5;
+            bool shipAhoy = true;
 
-            //ADDED
-            bool testbool = true;
-
-            CheckShip(size, randomYIndex, randomXIndex, direction, shipList, testbool);
+            CheckShip(size, randomYIndex, randomXIndex, direction, shipList, shipAhoy);
         }
 
-        private void CheckShip(int size, int randomYIndex, int randomXIndex, bool direction, List<Point> shipList, bool testbool)
+        private void CheckShip(int size, int randomYIndex, int randomXIndex, bool direction, List<Point> shipList, bool shipAhoy)
         {
             if ((direction && randomXIndex <= (gridSize - 1) - size) || (!direction && randomYIndex <= (gridSize - 1) - size))
             {
@@ -99,22 +96,19 @@ namespace BattleShipWpfApp
                     {
                         if (shipList.Contains(new Point(randomXIndex + i, randomYIndex)))
                         {
-                            testbool = false;
+                            shipAhoy = false;
                         }
                     }
 
-                    if (testbool)
+                    if (shipAhoy)
                     {
                         for (int j = 0; j < size; j++)
                         {
                             shipList.Add(new Point(randomXIndex + j, randomYIndex));
-                            
                         }
                     }
                     else
-                    {
                         CreateShip(size, shipList);
-                    }
                 }
                 else
                 {
@@ -122,12 +116,11 @@ namespace BattleShipWpfApp
                     {
                         if (shipList.Contains(new Point(randomXIndex, randomYIndex + i)))
                         {
-                            testbool = false;
-                            
+                            shipAhoy = false;
                         }
                     }
 
-                    if (testbool)
+                    if (shipAhoy)
                     {
                         for (int j = 0; j < size; j++)
                         {
@@ -135,36 +128,30 @@ namespace BattleShipWpfApp
                         }
                     }
                     else
-                    {
                         CreateShip(size, shipList);
-                    }
                 }
                 foreach (var ship in shipList)
                 {
                     int x = Convert.ToInt32(ship.X);
                     int y = Convert.ToInt32(ship.Y);
-                    _gridArray[x, y] = size.ToString();
+                    _gridArray[x, y] = /*size.ToString()*/ "Hit";
                 }
             }
             else
-            {
                 CreateShip(size, shipList);
-            }
         }
 
         private RoutedEventHandler GridClicked(int x, int y) //click event applied to all buttons via for loop
         {
             return (btn, e) =>
             {
-                //RevealAll();
-
                 _buttonArray[x, y].IsEnabled = false;
-
                 _buttonArray[x, y].Content = _gridArray[x, y];
-                if (_gridArray[x, y] != "Hit")
-                {
-                    ScoreBoard.Text = $"Number of misses: {_missCounter++.ToString()}";
-                }
+
+                if (_gridArray[x, y] != "Hit") ScoreBoard.Text = $"Number of misses: {_missCounter++.ToString()}";
+                else _hitCounter++;
+
+                if (_hitCounter == shipList.Count) RevealAll();
             };
         }
 
@@ -175,6 +162,7 @@ namespace BattleShipWpfApp
                 for (int y = 0; y < gridSize; y++)
                 {
                     _buttonArray[x, y].Content = _gridArray[x, y];
+                    _buttonArray[x, y].IsEnabled = false;
                 }
             }
         }
